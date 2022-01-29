9aa49b2789eb40c936a6365f0c177c32107781b7
Melissa2661 <melissa.katz@mail.mcgill.ca>
2022-01-29 11:05:30 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def main_wrapper():
+    print("Project start :)")
+    print(f"This is the function {main_wrapper.__name__}")
+    print("Project end :D")
+    pass
+
+
+if __name__ == "__main__":
+    main_wrapper()
